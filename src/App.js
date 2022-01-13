b80258e0b5a336daa7ab0d552fd4e2868d6ee853
Tenzin <tenthinten@gmail.com>
2022-01-13 18:52:23 +0530

    create different pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
 
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact component={Home} />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
+
+
+
+<Route path='/' exact component={Home} />
