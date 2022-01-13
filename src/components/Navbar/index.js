9405d6fab76e0191db1642f79312097f93e32ad8
Tenzin <tenthinten@gmail.com>
2022-01-13 12:33:55 +0530

    adding navbar contents

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+         <Nav>
+             <NavLink to="/">
+                 <h1>Logo</h1>
+             </NavLink>
+             <Bars />
+             <NavMenu>
+                 <NavLink to="/about" activeStyle>
+                     About
+                 </NavLink>
+                 <NavLink to="/services" activeStyle>
+                     Services
+                 </NavLink>
+                 <NavLink to="/contact-us" activeStyle>
+                     Contact Us
+                 </NavLink>
+                 <NavLink to="/sign-up" activeStyle>
+                     Sign Up
+                 </NavLink>
+             </NavMenu>
+         </Nav>
+        </>
+    )
+}
+
+export default Navbar
